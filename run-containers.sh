ecb82b71d5bd2fd83868fdcf11daa738ec799a1e
dvalo <dvalo@DESKTOP-S7CUAJ7>
2016-03-13 15:27:47 +0100

    commented

--- run-containers.sh
+++ run-containers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h consul --name consul progrium/consul -server -advertise $DOCKER_IP -bootstrap
+docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h consul --name consul progrium/consul -server -bootstrap
 docker run -d -v /var/run/docker.sock:/tmp/docker.sock --name registrator -h registrator gliderlabs/registrator:latest consul://$DOCKER_IP:8500
 docker run -d -P --name node1 -h node1 jlordiales/python-micro-service:latest
 docker run -d -P --name node2 -h node2 jlordiales/python-micro-service:latest
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 service nginx start
-consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload"
+#consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload"
