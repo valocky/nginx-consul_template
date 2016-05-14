6fcdfc625ea8823d7b1b35f502e621859e490620
Daniel <dvalo@DESKTOP-2FC7P2D>
2016-05-14 19:37:02 +0200

    background procesy

--- start.sh
+++ start.sh
@@ -4,11 +4,12 @@ JSON=`curl -XGET $CONSUL_URL/v1/catalog/service/elk-9200 |  jq '.[0].ServiceAddr
 
 RES="${JSON//\"/}"
 echo $RES
-
-curl -XPUT 'http://$RES:9200/_template/filebeat?pretty' -d@/usr/bin/filebeat.template.json
+ADR="http://$RES:9200/_template/filebeat?pretty"
+echo $ADR
+curl -XPUT $ADR -d@/usr/bin/filebeat.template.json
 
 service nginx start
 
-consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload"
+/usr/bin/consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload" &
 
-sudo ./usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e
+sudo /usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e &
