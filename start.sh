444a7bf41e12c3d6f130a2a088d922def077ac43
Daniel <dvalo@DESKTOP-2FC7P2D>
2016-05-14 20:11:00 +0200

    zakomentovane

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ ADR="http://$RES:9200/_template/filebeat?pretty"
 echo $ADR
 curl -XPUT $ADR -d@/usr/bin/filebeat.template.json
 
-service nginx start
+# service nginx start
 
-/usr/bin/consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload" &
-sudo /usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e &
+# /usr/bin/consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload" &
+# sudo /usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e &
 
