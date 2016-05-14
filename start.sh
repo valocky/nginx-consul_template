64a2560f0082d703084c6c96bc04b1c11f6e0295
Daniel <dvalo@DESKTOP-2FC7P2D>
2016-05-14 09:41:31 +0200

    premiestneny script

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@
 service nginx start
 
 consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload"
+sudo ./usr/bin/filebeat -e -c /usr/bin/filebeat.yml
