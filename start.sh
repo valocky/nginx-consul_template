94859775b31b5582a88f027b331264b62f5cf24a
Daniel <dvalo@DESKTOP-2FC7P2D>
2016-05-14 14:02:54 +0200

    pridany prikaz pre filebeat

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 service nginx start
 
 
-RUN curl -XPUT 'http://192.168.99.104:9200/_template/filebeat?pretty' -d@/usr/bin/filebeat.template.json
+curl -XPUT 'http://192.168.99.103:9200/_template/filebeat?pretty' -d@/usr/bin/filebeat.template.json
 
 consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload"
+
+sudo ./usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e
