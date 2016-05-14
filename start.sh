0a2d3e499568c0f99a7c4242f43ef99a69c6a922
Daniel <dvalo@DESKTOP-2FC7P2D>
2016-05-14 11:06:19 +0200

    odstranene spustienie filebeatu

--- start.sh
+++ start.sh
@@ -6,4 +6,3 @@ service nginx start
 RUN curl -XPUT 'http://192.168.99.104:9200/_template/filebeat?pretty' -d@/usr/bin/filebeat.template.json
 
 consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload"
-sudo ./usr/bin/filebeat /usr/bin/filebeat.yml -c -e
