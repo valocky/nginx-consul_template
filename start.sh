75f8bd8cb45d7d25520fcb1f369fecee156f1343
Daniel <dvalo@DESKTOP-2FC7P2D>
2016-05-14 17:09:36 +0200

    zmenene poradie

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-service nginx start
-
 JSON=`curl -XGET $CONSUL_URL/v1/catalog/service/elk-9200 |  jq '.[0].ServiceAddress'`
 
 RES="${JSON//\"/}"
 echo $RES
 
 curl -XPUT 'http://$RES:9200/_template/filebeat?pretty' -d@/usr/bin/filebeat.template.json
+
+service nginx start
 
 consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:service nginx reload"
 
